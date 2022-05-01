8ebc44557a99ff7f9d5958f3f0f7f781924956d6
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2022-05-01 13:57:42 +0100

    made button colors ditinct for each button

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,12 @@
 import './App.css';
 
 const Button = ({handle, name, color}) => {
+  let buttonn = 'button'
+  buttonn = color === 'red' ? `${buttonn} redd` : `${buttonn}`;
+  buttonn = color === 'blue' ? `${buttonn} bluee` : `${buttonn}`;
+  buttonn = color === 'brown' ? `${buttonn} brownn` : `${buttonn}`;
   return (
-  <button type='button' className='button' onClick={()=>{handle(color)}}>{name}</button>
+  <button type='button' className={buttonn} onClick={()=>{handle(color)}}>{name}</button>
 )}
 
 export default Button;
